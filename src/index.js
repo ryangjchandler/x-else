485a053349faadf7247b0fa4304fd0c3bfd46e06
Ryan Chandler <ryangjchandler@gmail.com>
2021-02-21 16:13:19 +0000

    feature: basic x-else support

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const AlpineElse = {
         if (! window.Alpine) {
             throw new Error('Alpine is require for `x-else` to work.')
         }
+
+        document.querySelectorAll('[x-else]').forEach(el => this.replaceElseDirectiveWithNegatedIf(el))
+    },
+    replaceElseDirectiveWithNegatedIf(el) {
+        const previous = el.previousElementSibling
+
+        if (! previous || previous.tagName.toLowerCase() !== 'template' || ! previous.hasAttribute('x-if')) {
+            throw new Error('`x-else` encountered without a previous `x-if` sibling.')
+        }
+
+        el.setAttribute('x-if', `! (${previous.getAttribute('x-if')})`)
     }
 }
 
